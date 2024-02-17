24c48d4af604302853afcfaf081a2ef277a571ec
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-02-17 15:25:50 +0100

    Added dynamic current year to footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,15 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="pt-5">
-      <p className="text-center">Footer here</p>
+      <p className="text-cente">
+        &copy; {currentYear}{" "}
+        <a href="./">
+          <strong>EcoRewards Hub</strong>
+        </a>
+        . All rights reserved.
+      </p>
     </footer>
   );
 }

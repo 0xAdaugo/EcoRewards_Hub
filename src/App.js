2f4798c869134dd29fdebce643f2a94819592b0d
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-02-18 14:56:03 +0100

    App touch up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 
 import Header from "./components/common/Header";
+import Home from "./components/pages/Home";
 import Footer from "./components/common/Footer";
 import SupportForm from "./components/common/SupportForm";
 
 function App() {
   return (
-    <div className="App container">
+    <div className="App container-fluid p-0">
       <Header />
+      <main>
+        <Home />
+      </main>
       <Footer />
       <SupportForm />
     </div>

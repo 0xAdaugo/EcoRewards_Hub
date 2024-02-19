5141addaa8afa4613c8ec2a162a8a9ffe8cb6ab6
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-02-19 08:36:25 +0100

    Added: Partners section.

---
+++ src/components/Partners.js
@@ -0,0 +1,11 @@
+function Partners() {
+  return (
+    <div className="partners py-5">
+      <div className="container p-0">
+        <h2 className="text-center mb-3">Our Partners</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Partners;

08611ea4b505f0c86e72405fe00998a786fa5b44
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-02-19 08:34:04 +0100

    Fixed 'Footer.js'.

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,15 +1,23 @@
+import siteLogo from "../../assets/img/ecoRewardsHub-logo.png";
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="pt-5">
-      <p className="text-cente">
-        &copy; {currentYear}{" "}
-        <a href="./">
-          <strong>EcoRewards Hub</strong>
-        </a>
-        . All rights reserved.
-      </p>
+    <footer className="py-5">
+      <div className="container">
+        <div className="text-center">
+          <p className="m-0">
+            <img alt="EcoRewards Hub" src={siteLogo} height="24" />
+            &nbsp; &copy; {currentYear}&nbsp;
+            <a href="./">
+              <strong>EcoRewards Hub</strong>
+            </a>
+            . <br className="d-md-none" />
+            All rights reserved.
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }

101bc2d435e63a3c953470b2bf29c37d8cd6d973
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-02-23 18:07:33 +0100

    Updated routes.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,21 @@
+import Home from "./components/pages/Home";
+import GetStarted from "./components/common/GetStarted";
+import Dashboard from "./components/pages/dashboard/Dashboard";
+
 export const routes = [
-    {
-        key: "1",
-        name: "Dashboard",
-    }
+  {
+    key: "1",
+    path: "/",
+    name: Home,
+  },
+  {
+    key: "2",
+    path: "/get-started",
+    name: GetStarted,
+  },
+  {
+    key: "3",
+    path: "/dashboard",
+    name: Dashboard,
+  },
 ];

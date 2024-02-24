951381ec3b0e6507e0632aedf503dad6a85c3169
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-02-24 20:22:43 +0100

    Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,20 @@
 import Hero from "./Hero";
 import OurProcesses from "../HowWeWork";
 import Partners from "../Partners";
+import { Helmet } from "react-helmet";
 
 function Home() {
   return (
-    <div className="container-fluid p-0">
-      <Hero />
-      <OurProcesses />
-      <Partners />
-    </div>
+    <>
+      <Helmet>
+        <title>EcoRewards Hub</title>
+      </Helmet>
+      <div className="container-fluid p-0">
+        <Hero />
+        <OurProcesses />
+        <Partners />
+      </div>
+    </>
   );
 }
 
